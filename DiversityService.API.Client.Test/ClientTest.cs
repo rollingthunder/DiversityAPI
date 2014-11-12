bbf2cb0e99b02dc90a2b52bbf0aca3e440c64f39
Georg Rollinger <rthundr@live.de>
2014-11-12 21:45:19 +0100

    Package Update, Client Project, Client Test Project

---
+++ DiversityService.API.Client.Test/ClientTest.cs
@@ -0,0 +1,26 @@
+﻿namespace DiversityService.API.Client.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class ClientTest
+    {
+        private const string USER = "Test";
+        private const string PASSWORD = "Pass";
+
+        private DiversityAPI Client = new DiversityAPI(USER, PASSWORD);
+
+        [Fact]
+        public void CanSetupClient()
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.NotNull(Client);
+        }
+    }
+}
---
+++ DiversityService.API.Client/DiversityAPI.cs
@@ -0,0 +1,14 @@
+﻿namespace DiversityService.API.Client
+{
+    using System;
+
+    public class DiversityAPI
+    {
+        private readonly string User;
+        private readonly string Password;
+
+        public DiversityAPI(string user, string password)
+        {
+        }
+    }
+}

f111e22bd345c1bed39e9b7bdcc74400d6c96338
Georg Rollinger <rthundr@live.de>
2015-07-09 22:39:56 +0200

    Add Taxa/-Factory Interfaces

---
+++ DiversityService.API.Interface/Taxa.cs
@@ -0,0 +1,19 @@
+﻿namespace DiversityService.API
+{
+    using DiversityService.API.Model;
+    using DiversityService.API.Model.Internal;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface ITaxaFactory
+    {
+        ITaxa GetTaxa(CollectionServerLogin login);
+    }
+
+    public interface ITaxa
+    {
+        Task<IEnumerable<TaxonList>> GetListsForUserAsync();
+
+        Task<IList<TaxonName>> GetTaxonNamesForList(int ListID, int skip = 0, int take = 50);
+    }
+}

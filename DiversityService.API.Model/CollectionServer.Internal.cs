a031c65ab97ac2754468f6a44ecd94561de462c3
Georg Rollinger <rthundr@live.de>
2015-07-09 17:25:01 +0200

    Implement IEquatable on CollectionServer Model Classes

--- DiversityService.API.Model/CollectionServer.Internal.cs
+++ DiversityService.API.Model/CollectionServer.Internal.cs
@@ -1,18 +1,35 @@
 ﻿namespace DiversityService.API.Model.Internal
 {
-    public class InternalCollectionServer : CollectionServer
+    using System;
+
+    public class InternalCollectionServer : CollectionServer, IEquatable<InternalCollectionServer>
     {
         public string Address { get; set; }
 
         public int Port { get; set; }
 
         public string Catalog { get; set; }
+
+        public bool Equals(InternalCollectionServer other)
+        {
+            return this.Equals(other as CollectionServer) &&
+                this.Address == other.Address &&
+                this.Port == other.Port &&
+                this.Catalog == other.Catalog;
+        }
     }
 
-    public class CollectionServerLogin : InternalCollectionServer
+    public class CollectionServerLogin : InternalCollectionServer, IEquatable<CollectionServerLogin>
     {
         public string User { get; set; }
 
         public string Password { get; set; }
+
+        public bool Equals(CollectionServerLogin other)
+        {
+            return this.Equals(other as InternalCollectionServer) &&
+                this.User == other.User &&
+                this.Password == other.Password;
+        }
     }
 }
--- DiversityService.API.Model/CollectionServer.cs
+++ DiversityService.API.Model/CollectionServer.cs
@@ -6,9 +6,17 @@
     using System.Text;
     using System.Threading.Tasks;
 
-    public partial class CollectionServer
+    public partial class CollectionServer : IEquatable<CollectionServer>
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
+
+        public bool Equals(CollectionServer other)
+        {
+            return other != null &&
+                this.Id == other.Id &&
+                this.Name == other.Name;
+        }
     }
 }
